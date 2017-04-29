3f69cdf5815eff9fe8f4366b1acc2ff8cb5adf7d
FamousMaxy <AMA@Ahmeds-MacBook-Pro.local>
2017-04-29 14:44:54 +0300

    First Commit

---
+++ MapTest/APIController (5).cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CO2Maps.Models;
+
+namespace CO2Maps.Controllers
+{
+    public class APIController : Controller
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+        // GET: API
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult AddMapDetails(string latitude,string longtitude, int value)
+        {
+            CO2Maps.Models.CO2Maps  mm = new Models.CO2Maps();
+            mm.Date = DateTime.UtcNow.AddHours(3);
+            mm.Latitude = latitude;
+            mm.Longtitude = longtitude;
+            mm.Value = value;
+            db.CO2Maps.Add(mm);
+            db.SaveChanges();
+            return Json(true, JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult GetMapDetails()
+        {
+            var mm = db.CO2Maps.Select(s=> new {ID = s.ID, Latitude = s.Latitude, Longtitude = s.Longtitude, Date = s.Date.ToShortDateString(), Time = s.Date.ToShortTimeString() }).ToList();
+            return Json(mm, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
--- MapTest/ViewController.swift
+++ MapTest/ViewController.swift
@@ -7,11 +7,26 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
 
+struct AnnotationData {
+    var Latitude:Double!
+    var Longtitude:Double!
+    var Value:Int!
+}
+
+class ViewController: UIViewController,MKMapViewDelegate {
+
+    @IBOutlet weak var mapView: MKMapView!
+    var Annotations:[AnnotationData] = []
+    
+    @IBOutlet weak var InformationLabel: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
+//        mapView.showsUserLocation = true
+        mapView.delegate = self
+        LoadAnnotations()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,6 +34,112 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
+
+    func SetAnnoteColor(Value: Int) -> UIImage {
+        switch Value {
+        case 390...392:
+            return #imageLiteral(resourceName: "Blue")
+        case 393...395:
+            return #imageLiteral(resourceName: "Cyan")
+        case 396...398:
+            return #imageLiteral(resourceName: "HighCyan")
+        case 399...402:
+            return #imageLiteral(resourceName: "Green")
+        case 403...420:
+            return #imageLiteral(resourceName: "Yellow")
+        case 421...470:
+            return #imageLiteral(resourceName: "Orange")
+        case 471...600:
+            return #imageLiteral(resourceName: "Red")
+        default:
+            return #imageLiteral(resourceName: "Black")
+        }
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        let annview = MKAnnotationView(annotation: annotation, reuseIdentifier: "pin")
+        annview.image = SetAnnoteColor(Value: Int(annotation.title!!)!)
+        annview.canShowCallout = true
+        
+        return annview
+
+    }
+    
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        if let annotation = view.annotation {
+            let ValueString = annotation.title!!
+            let valueInt = Int(ValueString)!
+            var MyText = ""
+            switch valueInt {
+            case 390...392:
+                MyText = "Your Life is safe here."
+            case 393...395:
+                MyText = "Air is fresh, live your life."
+            case 396...398:
+                MyText = "the Air is great, very suitable for living."
+            case 399...402:
+                MyText = "No threats, You are in work environment. You should take a walk around in the garden"
+            case 403...420:
+                MyText = "The air is polluted, you should plant some plants around"
+            case 421...470:
+                MyText = "Threaten, the air if very polluted. Please inform the authority now and run away"
+            case 471...600:
+                MyText = "Dangerous Area, leave now"
+            default:
+                MyText = "You can not live here"
+            }
+            InformationLabel.text = "CO2 = \(ValueString), \(MyText)"
+        }
+    }
+    
+    func LoadAnnotations() {
+        Annotations.removeAll(keepingCapacity: false)
+        let MainDomain = "http://co2maps.m1a1r.com/api/"
+        let session = URLSession.shared
+        var task = URLSessionDownloadTask()
+        
+        let url:URL! = URL(string: MainDomain + "GetMapDetails")
+        task = session.downloadTask(with: url, completionHandler: { (location: URL?, response: URLResponse?, error: Error?) -> Void in
+            if location != nil{
+                let data:Data! = try? Data(contentsOf: location!)
+                do{
+                    let JsonObjects = try JSONSerialization.jsonObject(with: data, options: .mutableLeaves) as? Array<Any>
+                    DispatchQueue.main.async {
+
+                    print(JsonObjects as Any)
+                        if JsonObjects?.count == 0 {
+                            print("No Test Points Found")
+                            return
+                        }
+                    for index in 0...(JsonObjects?.count)!-1 {
+                        
+                        var Annote = AnnotationData()
+                        let CurrentObject = JsonObjects?[index] as! Dictionary<String,Any>
+                        Annote.Latitude = Double(CurrentObject["Latitude"] as! String)
+                        Annote.Longtitude = Double(CurrentObject["Longtitude"] as! String)
+                        Annote.Value = CurrentObject["Value"] as! Int
+                        self.Annotations.append(Annote)
+                        print(Annote)
+                        let annotation = MKPointAnnotation()
+                        annotation.title = String(Annote.Value)
+                        annotation.coordinate = CLLocationCoordinate2D(latitude: Annote.Latitude, longitude: Annote.Longtitude)
+                        self.mapView.addAnnotation(annotation)
+                        
+                    }
+                    
+                    }
+                    
+                }catch{
+                    print("something went wrong, try again")
+                }
+            }
+        })
+        task.resume()
+    }
+    
+    
+    
 
 
 }
